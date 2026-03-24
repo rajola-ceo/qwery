9bb217c23215da41ce585c1790f85f84e28baa0d
rajola-ceo <v3nocyber@gmail.com>
2026-03-24 13:43:29 -0700

    Create index.js

---
+++ js/pages/index.js
@@ -0,0 +1,4 @@
+// js/pages/index.js
+export { ProfileSettingsPage } from './profileSettings.js';
+export { LeagueCreatePage } from './leagueCreate.js';
+export { LeagueViewPage } from './leagueView.js';
