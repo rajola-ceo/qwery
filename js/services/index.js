d2de04bb6f2b0bece2dc0fec1fefbde420e5c204
rajola-ceo <v3nocyber@gmail.com>
2026-03-25 08:30:29 -0700

    Delete js/services directory

--- js/services/index.js
+++
@@ -1,6 +0,0 @@
-// js/services/index.js
-export { authService } from './authService.js';
-export { userService } from './userService.js';
-export { coinService } from './coinService.js';
-export { leagueService } from './leagueService.js';
-export { teamService } from './teamService.js';
